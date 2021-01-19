1984c8dfc7be29f33e52abc463e6a4c46ddab977
Iwillgetmy <74886842+Iwillgetmy@users.noreply.github.com>
2021-01-18 20:27:09 -0500

    Add files via upload

---
+++ Flipper.java
@@ -0,0 +1,50 @@
+package POTW6;
+
+import java.util.Scanner;
+
+public class Flipper {
+
+	public static void main(String[] args) {
+		
+		//Scanner
+		Scanner keyboard = new Scanner(System.in);
+		
+		//Variables
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		int tempStore;
+		
+		//Input
+		String input = keyboard.nextLine();
+		
+		//Array to store characters
+		char characters[] = new char[input.length()];
+		
+		//getting each char
+		for(int i = 0; i<input.length(); i++) {
+			characters[i] = input.charAt(i);
+		}
+		
+		//Iterating through and doing task
+		for(char character: characters) {
+			if(character == 'H') {
+				 tempStore = a;
+				 a = c;
+				 c = tempStore;
+				 tempStore = b;
+				 b = d;
+				 d = tempStore;
+			} else {
+				tempStore = a;
+				a = b;
+				b = tempStore;
+				tempStore = c;
+				c = d;
+				d = tempStore;
+			}
+		}
+		System.out.println(a + " " + b + "\n" + c + " " + d);
+	}
+}
